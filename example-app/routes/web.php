f651040853dc8bee89173b6d16fcba8bc0a156f3
François-Xavier SAN JOSE <francois-xavier.sanjose@Digital-Grenoble.local>
2023-07-04 14:56:34 +0200

    Création du repo git laravel avec l'étape 2.1

---
+++ example-app/routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('Homepage');
+});
+
+
+
+Route::get('/product', function () {
+    return view('Liste des produits');
+});
+
+
+Route::get('/product/{id}', function () {
+    return view('Fiche du produit {id}');
+});
+
+
+Route::get('/cart', function () {
+    return view('Panier');
+});
+
