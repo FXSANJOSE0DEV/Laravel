2a11edeff9707e11429bd43236cf33e1afc09215
FXSANJOSE0DEV! <francois-xavier.sanjose@le-campus-numerique.fr>
2023-07-06 11:11:16 +0200

    Création de la v1 du layout (layout.blade.php). Modification du fichier product-list.blade.php. La page product-list s'affiche mais le css est ko. Il faut le fix avec la partie réaliser par Trichine.

--- example-app/resources/views/product-list.blade.php
+++ example-app/resources/views/product-list.blade.php
@@ -1,84 +1,83 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Page Produit</title>
-    <link rel="stylesheet" href="{{ asset('/css/css_product_page.css') }}">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
-    <link  href="{{ asset('assets/style.css')}}"rel="stylesheet">
-    <title>Galoper</title>
+@extends('layouts.layout')
+@section('title')
+    catalogue
+@endsection
+@section('body')
 
-</head>
-<body>
-    <div>
-    @include('includes.header')
-    </div>
+    <section>
+        <div class="product-container">
+            <div class="product-box">
+                <img
+                    src="https://www.equiphorse.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/i/m/image_5440.jpg"
+                    alt="Image 1">
+                <h2>Selle Western 1</h2>
+                <p>Description de l'article 1.</p>
+            </div>
 
-<section class="section1">
+            <div class="product-box">
+                <img
+                    src="https://www.kramer.fr/$WS/kraemer-pferdesport/websale8_shop-kraemer-pferdesport/produkte/medien/bilder/gross/B183216_MA.jpg"
+                    alt="Image 2">
+                <h2>Side-pull</h2>
+                <p>Description de l'article 2.</p>
+            </div>
 
-<div class="product-container ">
-    
-    <div class="product-box">
-        <img src="https://www.equiphorse.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/i/m/image_5440.jpg" alt="Image 1">
-        <h2>Selle Western 1</h2>
-        <p>Description de l'article 1.</p>
-    </div>
+            <div class="product-box">
+                <img src="https://www.equi-services.fr/3558-large_default/tapis-feutre-ronzon.jpg" alt="Image 3">
+                <h2>Tapis-feutre</h2>
+                <p>Description de l'article 3.</p>
+            </div>
 
-    <div class="product-box">
-        <img src="https://www.kramer.fr/$WS/kraemer-pferdesport/websale8_shop-kraemer-pferdesport/produkte/medien/bilder/gross/B183216_MA.jpg" alt="Image 2">
-        <h2>Side-pull</h2>
-        <p>Description de l'article 2.</p>
-    </div>
+            <div class="product-box">
+                <img
+                    src="https://www.laboutiqueducavalier.com/44545-large_default/masque-anti-mouche-anti-uv-cheval-de-trait.jpg"
+                    alt="Image 3">
+                <h2>Anti-mouche</h2>
+                <p>Description de l'article 3.</p>
+            </div>
 
-    <div class="product-box">
-        <img src="https://www.equi-services.fr/3558-large_default/tapis-feutre-ronzon.jpg" alt="Image 3">
-        <h2>Tapis-feutre</h2>
-        <p>Description de l'article 3.</p>
-    </div>
+            <div class="product-box">
+                <img
+                    src="https://lagrandesellerie.fr/262-large_default/casque-d-equitation-reference-antares-sellier.jpg"
+                    alt="Image 3">
+                <h2>Bombe</h2>
+                <p>Description de l'article 3.</p>
+            </div>
 
-    <div class="product-box">
-        <img src="https://www.laboutiqueducavalier.com/44545-large_default/masque-anti-mouche-anti-uv-cheval-de-trait.jpg" alt="Image 3">
-        <h2>Anti-mouche</h2>
-        <p>Description de l'article 3.</p>
-    </div>
+            <div class="product-box">
+                <img
+                    src="https://www.kramer.fr/$WS/kraemer-pferdesport/websale8_shop-kraemer-pferdesport/produkte/medien/bilder/gross/B280093_S.jpg"
+                    alt="Image 3">
+                <h2>Etriers</h2>
+                <p>Description de l'article 3.</p>
+            </div>
 
-    <div class="product-box">
-        <img src="https://lagrandesellerie.fr/262-large_default/casque-d-equitation-reference-antares-sellier.jpg" alt="Image 3">
-        <h2>Bombe</h2>
-        <p>Description de l'article 3.</p>
-    </div>
+            <div class="product-box">
+                <img src="https://www.mylookpro.com/3663-large_default/pantalon-de-travail-blanc-azurite.jpg"
+                     alt="Image 3">
+                <h2>Pantalon Griper</h2>
+                <p>Description de l'article 3.</p>
+            </div>
 
-    <div class="product-box">
-        <img src="https://www.kramer.fr/$WS/kraemer-pferdesport/websale8_shop-kraemer-pferdesport/produkte/medien/bilder/gross/B280093_S.jpg" alt="Image 3">
-        <h2>Etriers</h2>
-        <p>Description de l'article 3.</p>
-    </div>
+            <div class="product-box">
+                <img src="https://www.boutique-equitation.com/18178/pantalon-d-hiver-softshell-chaud-hkm.jpg"
+                     alt="Image 3">
+                <h2>Pantalon hiver</h2>
+                <p>Description de l'article 3.</p>
+            </div>
 
-    <div class="product-box">
-        <img src="https://www.mylookpro.com/3663-large_default/pantalon-de-travail-blanc-azurite.jpg" alt="Image 3">
-        <h2>Pantalon Griper</h2>
-        <p>Description de l'article 3.</p>
-    </div>
-
-    <div class="product-box">
-        <img src="https://www.boutique-equitation.com/18178/pantalon-d-hiver-softshell-chaud-hkm.jpg" alt="Image 3">
-        <h2>Pantalon hiver</h2>
-        <p>Description de l'article 3.</p>
-    </div>
-
-    <div class="product-box">
-        <img src="https://www.kramer.fr/$WS/kraemer-pferdesport/websale8_shop-kraemer-pferdesport/produkte/medien/bilder/gross/B810555_S.jpg" alt="Image 3">
-        <h2>Pantalon taille haute</h2>
-        <p>Description de l'article 3.</p>
-    </div>
+            <div class="product-box">
+                <img
+                    src="https://www.kramer.fr/$WS/kraemer-pferdesport/websale8_shop-kraemer-pferdesport/produkte/medien/bilder/gross/B810555_S.jpg"
+                    alt="Image 3">
+                <h2>Pantalon taille haute</h2>
+                <p>Description de l'article 3.</p>
+            </div>
 
 
+        </div>
+    </section>
 
-</div>
-@include('includes.footer')
-</section>
-
-</body>
-</html>
+@endsection
 
 
