660e5e91c40c7bc94dabf3ea87b462876e523426
FXSANJOSE0DEV! <francois-xavier.sanjose@le-campus-numerique.fr>
2023-07-06 11:11:16 +0200

    Création de la v1 du layout (layout.blade.php). Modification du fichier product-list.blade.php. La page product-list s'affiche mais le css est ko. Il faut le fix avec la partie réaliser par Trichine.

--- example-app/resources/views/layouts/layout.blade.php
+++ example-app/resources/views/layouts/layout.blade.php
@@ -6,21 +6,21 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
-    <link  href="{{ asset('assets/style.css')}}"rel="stylesheet">
-    <title>Galoper</title>
+    <link href="{{ asset('assets/style.css')}}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('/css/css_product_page.css') }}">
+    <title>@yield('title')</title>
+
 </head>
-    <!--le hedear-->
+<!--le hedear-->
 @include('includes.header')
-        <main>
-             <!--le contenu de la page-->
-            @yield('content')
-            
-        </main>
+<main>
+    @yield('content')
+    @yield('body')
+</main>
 
 
-        <footer>
-            <!--le footer-->
-        @include('includes.footer')
-        </footer>
-        
-    
+<footer>
+    <!--le footer-->
+    @include('includes.footer')
+</footer>
+
