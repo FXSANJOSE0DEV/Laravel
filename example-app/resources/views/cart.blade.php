1c971a306463272d7717b6c9795d836b3efedce9
FXSANJOSE0DEV! <francois-xavier.sanjose@le-campus-numerique.fr>
2023-07-06 14:36:08 +0200

    Add cart page, modify css file (css_product_page.css)

--- example-app/resources/views/cart.blade.php
+++ example-app/resources/views/cart.blade.php
@@ -1,3 +1,39 @@
-<?php
+@extends('layouts.layout')
+@section('content')
+<head>
+    <title>Mon Panier</title>
 
-echo "panier";
+</head>
+<body>
+<h1>Mon Panier</h1>
+
+<table>
+    <tr>
+        <th>Produit</th>
+        <th>Prix</th>
+        <th>Quantité</th>
+        <th>Total</th>
+    </tr>
+    <tr>
+        <td>Produit 1</td>
+        <td>19,99€</td>
+        <td>2</td>
+        <td>39,98€</td>
+    </tr>
+    <tr>
+        <td>Produit 2</td>
+        <td>9,99€</td>
+        <td>1</td>
+        <td>9,99€</td>
+    </tr>
+    <!-- Ajoutez d'autres lignes pour les autres produits dans le panier -->
+
+    <tr>
+        <td colspan="3" class="total">Total</td>
+        <td class="total">49,97€</td>
+    </tr>
+</table>
+
+<button>Valider la commande</button>
+@endsection
+</body>
