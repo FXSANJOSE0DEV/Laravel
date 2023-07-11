d0e8b9899c57d2ceea15370a4e011bfb7d606d2e
FXSANJOSE0DEV! <francois-xavier.sanjose@le-campus-numerique.fr>
2023-07-11 10:24:33 +0200

    Installing breeze Fix "web.php" file

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -1,11 +1,40 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+//use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
+
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\FormulairController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\BackOfficeController;
-use
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -37,12 +66,12 @@ Route::get('/formulair', [FormulairController::class, 'formulair']);
 Route::get('/BackOffice', [BackOfficeController::class, 'backoffice'])->name('backoffice');
 
 
-
 Route::get('/', function () {
     return view('Welcome');
 });
 
-Route::get('/dbconnect', function () {
+Route::get('/dbconnect', function (){
     return view('dbconnect');
 
 });
+
